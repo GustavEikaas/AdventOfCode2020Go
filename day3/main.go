33b9bae629ab32e616a15f6303c80f18072e15c0
Gustav <Gustav.eikaas@gmail.com>
2020-12-03 21:13:25 +0100

    refactor

--- day3/main.go
+++ day3/main.go
@@ -9,7 +9,7 @@ func main() {
 	/**
 	Part 1
 	 */
-	fmt.Println(runSlope(3,1))
+	fmt.Printf("Trees hit: %d%v", runSlope(3,1),"\n")
 	/**
 	Part 2
 	*/
@@ -17,17 +17,15 @@ func main() {
 	i := 1
 	for _, slope := range slopes {
 		i *= runSlope(slope[0],slope[1])
-
 	}
+	fmt.Printf("%v%d","Trees hit multiplied: " , i)
 	fmt.Println(i)
-
 }
 
 func runSlope(right int, down int)int {
 	input,_ := ioutil.ReadFile("input.txt")
 	var inputMap = strings.Split(string(input), "\n")
 	var treeCount,col int
-
 
 	for i :=0; i < len(inputMap)-1; i+= down {
 		if []rune(inputMap[i])[col] == '#' {
